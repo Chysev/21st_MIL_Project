e575679a302bb07d5cc857215b7a048b724f764e
Chysev <Johnlayda92@gmail.com>
2022-10-21 10:17:39 +0400

    Project

--- components/503/NotAvailable.tsx
+++ components/503/NotAvailable.tsx
@@ -1,10 +1,16 @@
 const NotAvailable = () => {
+
+    const date = new Date()
+
+    const dateObjects = {
+        day: date.getDate()
+    }
     return (
         <div className="flex min-h-[100vh] h-[100%] 992max:px-[2rem] BG">
             <div className="m-auto 992max:items-center 992max:grid">
-                <h1 className="text-2xl font-[900] text-white Time">Releasing World Wide | 7 Days Left</h1>
-                <h2 className="text-7xl 992max:text-4xl font-[900] text-white Time">October 28, 2022</h2>
-                <h2 className="text-2xl font-[900] text-white ml-[10rem] Time">100 Tula Para Kay Stella</h2>
+                <h1 className="text-2xl 992max:text-center font-[900] text-white Time">Releasing World Wide | Date: October {dateObjects.day}, 2022 </h1>
+                <h2 className="text-7xl 992max:text-center 992max:text-4xl font-[900] text-white Time">October 28, 2022</h2>
+                <h2 className="text-2xl 992max:text-center font-[900] text-white ml-[10rem] 992max:ml-[0] Time">100 Tula Para Kay Stella</h2>
             </div>
         </div>
     )
