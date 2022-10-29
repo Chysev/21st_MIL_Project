dcf55c7b2eb754c310c6b8c4b2ef63de89eefef5
Chysev <Johnlayda92@gmail.com>
2022-10-29 17:14:45 +0400

    Project

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default class CustomDocument extends Document {
                         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
                     />
 
-                    <meta
+                    {/*                     <meta
                         id="meta-application-name"
                         name="application-name"
                         content="PinoyTape"
@@ -19,14 +19,30 @@ export default class CustomDocument extends Document {
                     <meta
                         id="meta-description"
                         name="description"
-                        content="PinoyTape | 28.10.22 | 100 Tula Para Kay Stella | We are the production team, and we are dedicated to bringing you one of the best student films you have ever seen"
+                        content="
                     />
                     <meta
                         id="meta-keywords"
                         name="keywords"
                         content="Stella, Fidel, Fidel's Dad, Barloza, Chuck, Danica, Barrie, Sol, Hunter, Stella's Sister, Von, Rasul"
-                    />
+                    /> */}
 
+                    {/* Default */}
+                    <meta name="description" content="PinoyTape | 100 Tula Para Kay Stella | We are the production team, and we are dedicated to bringing you one of the best student films you have ever seen" />
+                    <meta name="image" content="../public/img/Background2.png" />
+
+                    {/* Schema Mark Up For Google */}
+                    <meta itemProp="name" content="PINOY TAPE " />
+                    <meta itemProp="description" content="PinoyTape | 100 Tula Para Kay Stella | We are the production team, and we are dedicated to bringing you one of the best student films you have ever seen" />
+                    <meta itemProp="image" content="../public/img/Background2.png" />
+
+                    {/* Open Graph Data */}
+                    <meta property="og:title" content="PINOY TAPE" />
+                    <meta property="og:type" content="PINOY TAPE" />
+                    <meta property="og:url" content="https://pinoytape.vercel.app/" />
+                    <meta property="og:image" content="../public/img/Background2.png" />
+                    <meta property="og:description" content="PinoyTape | 100 Tula Para Kay Stella | We are the production team, and we are dedicated to bringing you one of the best student films you have ever seen" />
+                    <meta property="og:site_name" content="PINOY TAPE" />
                 </Head>
 
                 <body>
